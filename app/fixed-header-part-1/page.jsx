eebc17e457f88ae2a48b08b9eefb6e069de11dab
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2023-10-20 22:03:39 -0700

    Animate Fixed Header

--- app/fixed-header-part-1/page.jsx
+++ app/fixed-header-part-1/page.jsx
@@ -1,8 +1,32 @@
+"use client";
+
+import {
+  motion,
+  useMotionValue,
+  useMotionValueEvent,
+  useScroll,
+} from "framer-motion";
+import { useEffect } from "react";
+
 export default function FixedHeaderPartOnePage() {
+  const { scrollY } = useScroll();
+  const height = useMotionValue(80);
+
+  useMotionValueEvent(scrollY, "change", (current) => {
+    let previous = scrollY.getPrevious();
+    let diff = current - previous;
+    let newHeight = height.get() - diff;
+
+    height.set(Math.min(Math.max(newHeight, 50), 80));
+  });
+
   return (
-    <div className="mx-auto flex w-full max-w-3xl flex-1 overflow-hidden text-slate-200 bg-gradient-to-br from-slate-700 to-slate-900">
+    <div className="mx-auto flex w-full max-w-3xl flex-1 overflow-hidden bg-gradient-to-br from-slate-700 to-slate-900 text-slate-600">
       <div className="z-0 flex-1 overflow-y-scroll">
-        <header className="flex h-20">
+        <motion.header
+          style={{ height }}
+          className="fixed inset-x-0 flex h-20 bg-white shadow"
+        >
           <div className="mx-auto flex w-full max-w-3xl items-center justify-between px-8">
             <p className="flex origin-left items-center text-xl font-semibold uppercase">
               <span className="-ml-1.5 inline-block -rotate-90 text-[10px] leading-[0]">
@@ -12,15 +36,15 @@ export default function FixedHeaderPartOnePage() {
                 Daily Bugle
               </span>
             </p>
-            <nav className="flex space-x-4 text-xs font-medium text-slate-50">
+            <nav className="flex space-x-4 text-xs font-medium text-slate-400">
               <a href="#">News</a>
               <a href="#">Sports</a>
               <a href="#">Culture</a>
             </nav>
           </div>
-        </header>
+        </motion.header>
 
-        <main className="px-8 pt-8">
+        <main className="px-8 pt-28">
           <h1 className="h-10 w-4/5 rounded bg-slate-200 text-2xl font-bold" />
           <div className="mt-8 space-y-6">
             {[...Array(2).keys()].map((i) => (
